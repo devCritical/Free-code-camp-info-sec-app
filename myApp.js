59cb8cd2198fcfe0b90ea25a590ec90ea4d445e7
devCritical <debian09jp@gmail.com>
2023-06-07 00:43:35 -0400

    Added BCrypt

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const app = express();
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 const noCache = require('nocache');
+const BCrypt = require('bcrypt');
 
 app.use(helmet.hidePoweredBy()); 
 app.use(helmet.frameguard({action: 'DENY'}));
